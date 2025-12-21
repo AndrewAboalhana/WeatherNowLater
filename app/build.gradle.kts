18e29cd80bf8948f6bdd4edb8e3bbf8925f5cf09
AndrewAboalhana <hana_hany6@yahoo.com>
2025-12-21 21:53:59 +0200

    update minimum SDK version to 28

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.aa.weathernowlater"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
